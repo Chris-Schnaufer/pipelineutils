e3df9f746366ac13ede6611d57567f6af67a0866
chris-schnaufer <schnaufer@email.arizona.edu>
2019-06-17 22:11:59 +0000

    Working on tests

---
+++ test/.ipynb_checkpoints/extract-checkpoint.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+set -ev
+
+# Make the folder for the data files
+mkdir data
+
+# Copy the TAR file
+cp "$1" data/
+cd  data
+
+# Decompress the tar file
+TAR_FILENAME="$1"
+if [[ ! -f "./${TAR_FILENAME}" ]]; then
+    # The copied file doesn't exist - possibly due to folder structure - so we try to find it
+    for fn in *.tar; do
+        [[ -f "${fn}" ]] || break
+        echo "Checking if file ${fn} matches ${TAR_FILENAME}"
+        if [[ "${TAR_FILENAME}" == *"${fn}" ]]; then
+            TAR_FILENAME="${fn}"
+            break
+        fi
+    done
+fi
+if [[ ! -f "${TAR_FILENAME}" ]]; then
+    echo "Unable to find file to decompress, no files decompressed"
+    exit 0
+fi
+tar -xv -f "${TAR_FILENAME}"
+
+# Remove the tar file copy
+rm "${TAR_FILENAME}"
