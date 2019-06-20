1ff472cb4498b7ac41f863c00753f4d05856816a
schnaufer <schnaufer@email.arizona.com>
2019-06-20 22:36:34 +0000

    Bumping version number

--- pipelineutils/setup.py
+++ pipelineutils/setup.py
@@ -10,7 +10,7 @@ def description():
 
 setup(name='pipelineutils',
       packages=find_packages(),
-      version='1.0.3',
+      version='1.0.4',
       include_package_data=True,
       description='Drone pipeline workflow utilities',
       long_description=description(),
